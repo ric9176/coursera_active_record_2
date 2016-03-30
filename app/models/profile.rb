3e87fe63f0313db425e1081d5321a8dd5292ec8e
Richard Moss <richard@englishtutorslive.com>
2016-03-30 15:20:08 +0100

    adds validations to User and Profile models and passes test 11 as per spec

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,21 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  validates :gender, inclusion: { in: %w(male female), message: "%{value} is not a vaild gender" }
+  validate :names_not_empty
+  validate :not_a_boy_named_sue
+
+  def names_not_empty
+    if first_name.nil? && last_name.nil?
+      errors.add(:first_name, "Please enter either your first or last name or both")
+      errors.add(:last_name, "Please enter either your first or last name or both")
+    end
+  end
+
+  def not_a_boy_named_sue
+    if gender == "male" && first_name == "Sue"
+      errors.add(:gender, "wrong gender")
+    end
+  end
+
+
 end
